517f14137a1ec8ea5f22ab5d04ef9fe28d6b0a61
Siemowit Bialkowski <ballzhamon@gmail.com>
2014-04-20 12:18:24 +0200

    Bez kontekstu bazy i listy jakichś wpisów to trochę abstrakcyjne, ale póki co jak się gdzieś wstawi link "@Html.ActionLink("Item", "Index", "Item")" to wyświetla listę intów - tam będą wpisy - nie robiłem obsługi delete/edit/create pod te inty, bo i po co. Zrobione na klasie i kontekście UserProfile, trzeba by to zamienić na odpowiedną klasę i kontekst odnośnie wpisów.

---
+++ VideoGamesClub/Controllers/ItemController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoGamesClub.Models;
+
+namespace VideoGamesClub.Controllers
+{
+    public class ItemController : Controller
+    {
+        private UsersContext db = new UsersContext(); // kontekst do bazy
+        private List<int> ItemList = new List<int>(); // lista uzyskana z bazy
+        //
+        // GET: /Item/
+
+        public ActionResult Index()
+        {
+            for(int i=0;i<10;i++)
+                ItemList.Add(i);
+            return View(ItemList); //db.UserProfiles.ToList()
+        }
+
+        //
+        // GET: /Item/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /Item/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Item/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfiles.Add(userprofile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userprofile);
+        }
+
+        //
+        // GET: /Item/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /Item/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userprofile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /Item/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /Item/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            db.UserProfiles.Remove(userprofile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
