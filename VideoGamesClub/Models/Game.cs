bbf08e372e6bd4f591f452a78d2f2db6b396b48d
miczar7 <mujczar@wp.pl>
2014-04-26 22:30:02 +0200

    dodanie modeli do VideoGamesClub\Models
    utworzenie modeli Game i User oraz dodanie klasy VideoGamesClubDbContext

---
+++ VideoGamesClub/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoGamesClub.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public int UserId { get; set; }
+        public DateTime IntroduceDate { get; set; }
+    }
+}
---
+++ VideoGamesClub/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoGamesClub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Nick { get; set; }
+        public string Email { get; set; }
+        public string Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int PasswordHash { get; set; }
+    }
+}
